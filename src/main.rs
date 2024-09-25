12b7ab38fbc45fb42ff03821f95072e51b30837b
veqcc <ryuta.kambe@tier4.jp>
2024-09-25 19:37:48 +0900

    remove
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Mutex;
-use once_cell::sync::Lazy;
-
-static GLOBAL : Lazy<Mutex<u32>> = Lazy::new(|| Mutex::new(1));
-
-fn main() {
-    let data = GLOBAL.lock().unwrap();
-    println!("global = {}", *data);
-}
