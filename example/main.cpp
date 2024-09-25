23d61431de8632eca0ac4ce8b8a2999175e80adf
veqcc <ryuta.kambe@tier4.jp>
2024-09-25 15:59:34 +0900

    stable
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

---
+++ example/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::vector<int> vec = {};
+    int sum = 0;
+
+    for (int i = 0; i < 100000; i++) {
+        vec.push_back(i);
+        sum += i;
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 use std::env;
 use std::process;
 use lazy_static::lazy_static;
@@ -7,19 +7,32 @@ use std::ptr;
 use libc::{mmap, PROT_READ, PROT_WRITE, MAP_PRIVATE, MAP_ANONYMOUS};
 use rlsf::Tlsf;
 use std::mem::MaybeUninit;
+use once_cell::sync::Lazy;
+use std::os::raw::c_void;
+use libc::{size_t, dlsym, RTLD_NEXT};
+use std::ffi::CStr;
+
+static MEMPOOL_INITIALIZED : AtomicBool = AtomicBool::new(false);
+lazy_static! {
+    static ref MUTEX : Mutex<bool> = Mutex::new(false);
+}
+
+type MallocType = unsafe extern "C" fn(size_t) -> *mut c_void;
+static ORIGINAL_MALLOC : Lazy<MallocType> = Lazy::new(|| {
+    unsafe {
+        let symbol = CStr::from_bytes_with_nul(b"malloc\0").unwrap();
+        let malloc_ptr = dlsym(RTLD_NEXT, symbol.as_ptr());
+        std::mem::transmute(malloc_ptr)
+    }
+});
 
 fn initialize() -> () {
-    lazy_static! {
-        static ref MUTEX : Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
-    }
-    
-    static MEMPOOL_INITIALIZED : AtomicBool = AtomicBool::new(false);
-
     if MEMPOOL_INITIALIZED.load(Ordering::Acquire) {
         return;
     }
 
     let _guard = MUTEX.lock().unwrap();
+
 
     if MEMPOOL_INITIALIZED.load(Ordering::Acquire) {
         return;
@@ -59,35 +72,21 @@ fn initialize() -> () {
     MEMPOOL_INITIALIZED.store(true, Ordering::Release);
 }
 
-
-use std::os::raw::c_void;
-use libc::{size_t, dlsym, RTLD_NEXT};
-use std::ffi::CStr;
-
-type MallocType = unsafe extern "C" fn(size_t) -> *mut c_void;
-static mut ORIGINAL_MALLOC: Option<MallocType> = None;
-
-fn get_original_malloc() {
-    unsafe {
-        let symbol = CStr::from_bytes_with_nul(b"malloc\0").unwrap();
-        let malloc_ptr = dlsym(RTLD_NEXT, symbol.as_ptr());
-        ORIGINAL_MALLOC = Some(std::mem::transmute(malloc_ptr));
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn malloc(size : size_t) -> *mut c_void {
+    static mut IS_IN_HOOKED_CALL : bool = false;
+    
     unsafe {
-        if let Some(orig) = ORIGINAL_MALLOC {
-            orig(size)
+        // let original_malloc = ORIGINAL_MALLOC.unwrap();
+        if IS_IN_HOOKED_CALL {
+            ORIGINAL_MALLOC(size)
         } else {
-            println!("malloc failed");
-            process::exit(1)
+            IS_IN_HOOKED_CALL = true;
+            initialize();
+            let ret = ORIGINAL_MALLOC(size);
+            IS_IN_HOOKED_CALL = false;
+
+            ret
         }
     }
-}
-
-fn main() {
-    get_original_malloc();
-    initialize();
 }
